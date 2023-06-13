c4dc6d52a5bd87ef50ddf6bd5d7abe14d2dfc3d7
grs-se <george_ricesmith@hotmail.com>
2023-06-13 19:05:18 +0100

    Add edit handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,21 +9,27 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            // Send query to mediator handler
             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            // Object initializer syntax { } = Id set to id when class is initialised
             return await Mediator.Send(new Details.Query { Id = id });
         }
-     
+
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+
     }
 }
