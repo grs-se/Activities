25df6c36368b8c648d37df5512e316ebc54a0a54
grs-se <george_ricesmith@hotmail.com>
2023-06-14 17:57:09 +0100

    Use GUID for activity id

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { v4 as uuid } from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -37,7 +38,8 @@ function App() {
   function handleCreateOrEditActivity(activity: Activity) {
     activity.id
       ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, activity]);
+      // create id at same time as creating new activity
+      : setActivities([...activities, {...activity, id: uuid()}]);
     setEditMode(false);
     setSelectedActivity(activity);
   }
