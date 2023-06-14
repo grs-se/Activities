dc664e0467b77cefd5af04a0c7e9c7c0cd5ac69f
grs-se <george_ricesmith@hotmail.com>
2023-06-14 17:49:12 +0100

    Handle create and edit submission

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,9 +5,10 @@ import { Activity } from '../../../app/models/activity';
 interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
 }
 
-function ActivityForm({ activity: selectedActivity, closeForm }: Props) {
+function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit }: Props) {
 
   const initialState = selectedActivity ?? {
     id: '',
@@ -22,7 +23,7 @@ function ActivityForm({ activity: selectedActivity, closeForm }: Props) {
   const [activity, setActivity] = useState(initialState);
 
   function handleSubmit() {
-    console.log(activity);
+    createOrEdit(activity);
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
