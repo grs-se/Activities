c2b50786fee477e350fdd263fe4eec9bff440544
grs-se <george_ricesmith@hotmail.com>
2023-06-15 14:30:24 +0100

    Add edit activity route

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,18 @@
 import { observer } from 'mobx-react-lite';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import { Button, Form, Segment } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
-  const { selectedActivity, createActivity, updateActivity, loading } = activityStore;
- 
-  const initialState = selectedActivity ?? {
+  const { createActivity, updateActivity,
+    loading, loadActivity, loadingInitial } = activityStore;
+  const { id } = useParams();
+
+  const [activity, setActivity] = useState<Activity>({
     id: '',
     title: '',
     category: '',
@@ -15,9 +20,11 @@ export default observer(function ActivityForm() {
     date: '',
     city: '',
     venue: ''
-  }
+  });
 
-  const [activity, setActivity] = useState(initialState);
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!))
+  }, [id, loadActivity])
 
   function handleSubmit() {
     activity.id ? updateActivity(activity) : createActivity(activity);
@@ -27,6 +34,8 @@ export default observer(function ActivityForm() {
     const { name, value } = event.target;
     setActivity({...activity, [name]: value})
   }
+
+  if (loadingInitial) return <LoadingComponent content='Loading activity...'/>
 
   return (
     <Segment clearing>
