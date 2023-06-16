f51afc7ee3f4cf35defcff08d8b023ea4e75017e
grs-se <george_ricesmith@hotmail.com>
2023-06-16 22:03:10 +0100

    Prepare for setting up error handling in client

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouteObject } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import TestErrors from "../../features/errors/TestError";
 import App from "../layout/App";
 
 export const routes: RouteObject[] = [
@@ -13,6 +14,7 @@ export const routes: RouteObject[] = [
       { path: 'activities/:id', element: <ActivityDetails/>},
       { path: 'createActivity', element: <ActivityForm key='create' />},
       { path: 'manage/:id', element: <ActivityForm key='manage' />},
+      { path: 'errors', element: <TestErrors key='manage' />},
     ]
   }
 ]
