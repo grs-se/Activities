b29edaf855c254f9dc685ebcb6af5981f44d9774
grs-se <george_ricesmith@hotmail.com>
2023-06-16 15:41:41 +0100

    Add Fluent Validation

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Activities;
 using Application.Core;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -26,6 +28,8 @@ namespace API.Extensions
             });
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<List.Handler>());
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
 
             return services;
         }
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -11,6 +12,14 @@ namespace Application.Activities
             public Activity Activity { get; set; }
         }
 
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
         public class Handler : IRequestHandler<Command, Unit>
         {
             private readonly DataContext _context;
@@ -19,14 +28,11 @@ namespace Application.Activities
             {
                 _context = context;
             }
-            // Unit doesn't actually have any value, like returning nothing, but does tell API that our request is finished so it can move on.
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                // add activity to memory, we do not touch db, so we do not need to use the async version of Add method
                 _context.Activities.Add(request.Activity);
 
                 await _context.SaveChangesAsync();
-                // equivalent to nothing, but usefult let API controller know we are finished. 
                 return Unit.Value;
             }
         }
