de8b304e30a8a6e4466fefb6a742a0165df2d694
grs-se <george_ricesmith@hotmail.com>
2023-06-17 15:52:29 +0100

    Hook up form submission to Formik

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,13 +5,12 @@ import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import { ToastContainer } from 'react-toastify';
 
-
 function App() {
   const location = useLocation();
 
   return (
     <>
-    <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       {location.pathname === '/' ? <HomePage /> : (
         <>
           <NavBar />
@@ -19,8 +18,7 @@ function App() {
             <Outlet />
           </Container>
         </>
-      ) }
-      
+      )}
     </>
   );
 }
