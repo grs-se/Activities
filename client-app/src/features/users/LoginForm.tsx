454050f6ca3284da658e173cafad76e22cdae019
grs-se <george_ricesmith@hotmail.com>
2023-06-18 16:23:13 +0100

    Add modals

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Form, Formik } from 'formik';
 import { observer } from 'mobx-react-lite';
-import { Button, Label } from 'semantic-ui-react';
+import { Button, Header, Label } from 'semantic-ui-react';
 import MyTextInput from '../../app/common/form/MyTextInput';
 import { useStore } from '../../app/stores/store';
 
@@ -15,12 +15,13 @@ export default observer(function LoginForm() {
 		>
 			{({ handleSubmit, isSubmitting, errors }) => (
 				<Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+					<Header as='h2' content='Login to Activities' color='teal' textAlign='center' />
 					<MyTextInput placeholder='Email' name='email' />
 					<MyTextInput placeholder='Password' name='password' type='password' />
 					<ErrorMessage
 						name='error' render={() =>
 							<Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
-						/>
+					/>
 					<Button loading={isSubmitting} positive content='Login' type='submit' fluid />
 				</Form>
 			)}
