7475ecb2f90a75aa3ba5fcb36268fa56e4acddcc
grs-se <george_ricesmith@hotmail.com>
2023-06-18 11:20:45 +0100

    Create auth policy

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,11 +1,14 @@
 ﻿using API.DTOs;
 using API.Services;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    // AllowAnonymous: none of these endpoints require authentication
+    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
