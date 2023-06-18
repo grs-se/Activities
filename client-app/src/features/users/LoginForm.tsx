c2539aca511655b47c759fabe458bc9066d92233
grs-se <george_ricesmith@hotmail.com>
2023-06-18 13:37:42 +0100

    Create user store

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,22 +1,25 @@
 import { Form, Formik } from 'formik';
-import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { Button } from 'semantic-ui-react';
 import MyTextInput from '../../app/common/form/MyTextInput';
+import { useStore } from '../../app/stores/store';
 
-export default function LoginForm() {
-  return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      onSubmit={values=> console.log(values)}
-    >
-      {({ handleSubmit }) => (
-        <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-          <MyTextInput placeholder='Email' name='email' />
-          <MyTextInput placeholder='Password' name='password' type='password' />
-          <Button positive content='Login' type='submit' fluid />
-        </Form>
-      ) }
+export default observer(function LoginForm() {
+	const { userStore } = useStore();
 
-    </Formik>
-  );
-}
+	return (
+		<Formik
+			initialValues={{ email: '', password: '' }}
+			onSubmit={values => userStore.login(values)}
+		>
+			{({ handleSubmit, isSubmitting }) => (
+				<Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+					<MyTextInput placeholder='Email' name='email' />
+					<MyTextInput placeholder='Password' name='password' type='password' />
+					<Button loading={isSubmitting} positive content='Login' type='submit' fluid />
+				</Form>
+			)}
+
+		</Formik>
+	);
+});
