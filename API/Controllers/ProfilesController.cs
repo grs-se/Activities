c6d4078fa059817a81a68350ef80a41f282fbeb2
grs-se <george_ricesmith@hotmail.com>
2023-06-20 21:19:24 +0100

    Update the mapping configuration

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,6 +1,14 @@
-﻿namespace API.Controllers
+﻿using Application.Profiles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
 {
-    public class ProfilesController
+    public class ProfilesController : BaseApiController
     {
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
     }
 }
