71353bf7d3d82d1dfb7be53c3e0f758aab9c1632
grs-se <george_ricesmith@hotmail.com>
2023-06-20 13:49:57 +0100

    Add Cloudinary interfaces

---
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -0,0 +1,12 @@
+﻿using Application.Photos;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Interfaces
+{
+    public interface IPhotoAccessor
+    {
+        // nothing to do with DB, purely related to uploading and deleting from Cloudinary.
+        Task<PhotoUploadResult> AddPhoto(IFormFile file);
+        Task<string> DeletePhoto(string publicId);
+    }
+}
