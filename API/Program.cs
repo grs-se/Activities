021a05c33eb6738ee8e06fd5b29d91ba5c838d02
grs-se <george_ricesmith@hotmail.com>
2023-06-22 11:22:49 +0100

    Authenticate to SignalR

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -39,6 +40,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<ChatHub>("/chat");
 
 // using: when we are finished executing this particular method / scope anything inside it will be disposed and cleaned from memory.
 using var scope = app.Services.CreateScope();
